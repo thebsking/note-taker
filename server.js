cc850ce5d144f765414485369a48f8615bbb8179
Brandon King <brandonscottking@gmail.com>
2021-04-06 22:03:44 -0400

    corrected index.js based on update to class repo

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //set dependencies
+const { static } = require('express');
 const express = require('express');
 const path = require('path');
 
+//app setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'))
 
+//html routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 
+//api routes
 
 
 //start server
