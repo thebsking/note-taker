acba503cab4d7e61518a74d42669311553800a5f
Brandon King <brandonscottking@gmail.com>
2021-04-11 17:43:41 -0400

    began api functions

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const { static } = require('express');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const notesDb = './db/db.json'
 
 //app setup
 const app = express();
@@ -15,7 +17,14 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 
 //api routes
+app.get('/api/notes', (req, res) => {
+    
+});
 
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.appendFile(notesDb, newNote)
+})
 
 //start server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
