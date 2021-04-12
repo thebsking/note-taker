ddf27428d83bd93bfcd095f4e48bd41564a15f75
Brandon King <brandonscottking@gmail.com>
2021-04-12 11:06:26 -0400

    attempting to display db.json content

--- server.js
+++ server.js
@@ -3,27 +3,37 @@ const { static } = require('express');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notesDb = './db/db.json'
+const notesDbPath = './db/db.json'
 
 //app setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
 //html routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 
+let notesDbContent = [];
+fs.readFile(notesDbPath, 'utf8', (err, data)=>{
+    if(err) throw err;
+    notesDbContent += data;
+});
+
 //api routes
 app.get('/api/notes', (req, res) => {
-    
+    fs.readFile(notesDbPath, 'utf8', (err, data)=>{
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    fs.appendFile(notesDb, newNote)
+    fs.appendFile(notesDbPath, JSON.stringify(req.body), (err) => {
+        if (err) throw (err);
+    })
 })
 
 //start server
